41d07eb848818f17f67cac620850d87169eb90cd
ncantarelli <necantarelli@gmail.com>
2023-11-03 10:42:41 +0100

    Making credentials a bit safer

--- server/config/cloudinaryConfig.js
+++ server/config/cloudinaryConfig.js
@@ -4,11 +4,8 @@ import { v2 as cloudinary } from 'cloudinary';
 const cloudinaryConfig = () => {
 
     cloudinary.config({
-        // cloud_name: 'dykwqjdq3',
         cloud_name: process.env.CLOUD_NAME,
-        // api_key: '891171895987986',
         api_key: process.env.API_KEY,
-        // api_secret: "XSfLZUTyDOxKEXHCsPGjjOO0x1I",
         api_secret: process.env.API_SECRET,
         secure: true,
     });
